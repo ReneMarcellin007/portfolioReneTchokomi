80546b659356ed066590d66f86edc344905b4300
RENÉ-MARCELLIN TCHOKOMI <2136882@CegepGranby.qc.ca>
2024-07-31 22:00:10 -0400

    Configure Nodemailer with environment variables

--- app/contact/api/send-email.js
+++ app/contact/api/send-email.js
@@ -1,41 +1,37 @@
-// pages/api/send-email.js
-import nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    }
+});
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { firstname, lastname, email, phone, message } = req.body;
 
-        // Configurer le transporteur SMTP
-        let transporter = nodemailer.createTransport({
-            host: 'smtp-relay.brevo.com',
-            port: 587,
-            secure: false, // true pour le port 465, false pour les autres ports
-            auth: {
-                user: '79a959001@smtp-brevo.com', // remplacez par votre login SMTP Brevo
-                pass: 'WVwLbZXsG6pS4Omh', // remplacez par votre mot de passe SMTP Brevo
-            },
-        });
-
-        // Options de l'email
-        let mailOptions = {
-            from: '"René-Marcellin" <79a959001@smtp-brevo.com>', // adresse email vérifiée comme expéditeur
-            to: 'tchokorerene@gmail.com', // liste des destinataires
-            subject: `Message de ${firstname} ${lastname}`,
+        const mailOptions = {
+            from: process.env.SMTP_USER,
+            to: 'tchokorerene@gmail.com',
+            subject: `Message from ${firstname} ${lastname}`,
             text: `
-        Prénom: ${firstname}
-        Nom: ${lastname}
-        Email: ${email}
-        Téléphone: ${phone}
-        Message: ${message}
-      `,
+                First Name: ${firstname}
+                Last Name: ${lastname}
+                Email: ${email}
+                Phone: ${phone}
+                Message: ${message}
+            `,
         };
 
-        // Envoyer l'email
         try {
             await transporter.sendMail(mailOptions);
             res.status(200).json({ message: 'Email sent successfully' });
         } catch (error) {
-            console.error(error);
+            console.error('Error sending email:', error);
             res.status(500).json({ error: 'Error sending email' });
         }
     } else {
